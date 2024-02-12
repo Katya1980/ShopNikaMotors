cf1b7b25aec3749658e4d6b2a624922d5414e906
Lenovo <nikamotors2020@mail.ru>
2024-02-12 13:11:47 +0300

    Сериализация

--- src/main/java/by/teachmeslills/nikamotors/repository/FileRepository.java
+++ src/main/java/by/teachmeslills/nikamotors/repository/FileRepository.java
@@ -2,26 +2,71 @@ package by.teachmeslills.nikamotors.repository;
 
 import by.teachmeslills.nikamotors.entity.User;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.OptionalLong;
 
 public class FileRepository implements ShopRepository {
+    private final String filePath = "E:\\Козловская Е. В\\ShopNikaMotors\\usersfile";
+    private List<User> users;
+
+    public FileRepository() {
+        users = deserializable();
+    }
+
 
     @Override
     public void add(User user) {
+        users.add(user);
+        serializable();
 
     }
+
 
     @Override
-    public void deleteByld(Long userId) {
+    public void deleteById(Long userId) {
 
+
+        serializable();
     }
+
 
     @Override
     public Collection<User> allUsers() {
-        return null;
+        return users;
     }
-public void Serializable (){
+
+
+    public void serializable() {
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            os.writeObject(users);
+            System.out.println("Сериализация успешна. Количество пользователей: " + users.size());
+        } catch (IOException e) {
+            System.err.println("Ошибка при сериализации пользователей: " + e.getMessage());
+
+        }
+
+    }
+
+    public List<User> deserializable() {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+        try (ObjectInputStream is = new ObjectInputStream(new FileInputStream(file))) {
+            return (List<User>) is.readObject();
+
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Ошибка при десериализации пользователей: " + e.getMessage());
+
+        }
+
+        return new ArrayList<>();
+    }
+
 
 }
-}
+
